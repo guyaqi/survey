aaee3a5cb52f3d0ae74fcd4ebdbb3305b20f9e47
guyaqi <guyaqi5858@outlook.com>
2019-03-04 17:29:38 +0800

    m

--- dbscript/db_init.sql
+++ dbscript/db_init.sql
@@ -1,26 +1,3 @@
-/*
-
-let surveyForm = {
-    age:0,
-    FirstLanguage:"",
-    dailyLanguage:"",
-    startEnglish:0,
-    Education:0,
-    way:"",
-    wayOther:"",
-    live:0,
-    long:0,
-    rate:0,
-    reading:"",
-    readingOthers:"",
-    t:[],
-    a:[]
-}
-
-*/
-
-/* assuming that the name of DATABASE is "forms" */
-
 CREATE TABLE IF NOT EXISTS `lang_survey`(
    `id` INT AUTO_INCREMENT,
    `age` INT NOT NULL,
